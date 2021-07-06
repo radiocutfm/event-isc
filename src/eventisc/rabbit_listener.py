4e31c643f128874d71ef5ad65c55d1d14d3a0458
Emiliano Carrizo Vega <emi.carrizo94@gmail.com>
2021-07-06 14:48:52 -0300

    use of threading.local to save rabbit channel per thread and test refactoring

--- src/eventisc/rabbit_listener.py
+++ src/eventisc/rabbit_listener.py
@@ -3,31 +3,37 @@ from __future__ import absolute_import
 from . import Listener
 import pika
 import json
+import threading
+
+local = threading.local()
 
 
 class RabbitListener(Listener):
     kind = "rabbit"
 
-    def __init__(self, event_name, url, queue_kwargs=None, data=None, filter=None):
+    def _connect(self):
+        if not hasattr(local, "channel"):
+            connection = pika.BlockingConnection(pika.URLParameters(self.url))
+            local.channel = connection.channel()
+            queue = self.queue_kwargs.get("queue", {})
+            local.channel.queue_declare(**queue)
+            exchange = self.queue_kwargs.get("exchange", {})
+            if exchange:
+                local.channel.exchange_declare(**exchange)
+                local.channel.queue_bind(exchange=exchange.get("exchange"), queue=queue.get("queue"))
+
+    def __init__(self, event_name, url, queue_kwargs=None, publish_kwargs=None, filter=None):
         super(RabbitListener, self).__init__(event_name, filter)
         self.url = url
         self.queue_kwargs = queue_kwargs
-        self.data = data
-        queue = self.queue_kwargs.get("routing_key")
-        connection = pika.BlockingConnection(pika.URLParameters(self.url))
-        self.channel = connection.channel()
-        self.channel.queue_declare(queue)
-        exchange = self.queue_kwargs.get("exchange")
-        if exchange:
-            self.channel.exchange_declare(exchange=exchange, exchange_type='direct')
-            self.channel.queue_bind(exchange=exchange, queue=queue)
+        self.publish_kwargs = publish_kwargs
+        self._connect()
 
     def _do_notify(self, event_name, event_data):
-        if self.data:
-            data = dict((k, self.format(v, event_name, event_data))
-                        for (k, v) in self.data.items())
+        if self.publish_kwargs:
+            data = dict((k, self.format(v, event_name, event_data, queue_kwargs=self.queue_kwargs))
+                        for (k, v) in self.publish_kwargs.items())
         else:
             data = None
-        publish_kwargs = dict(self.queue_kwargs)
-        publish_kwargs["body"] = json.dumps(data)
-        self.channel.basic_publish(**publish_kwargs)
+        data["body"] = json.dumps(event_data)
+        local.channel.basic_publish(**data)
--- tests/test_rabbit.py
+++ tests/test_rabbit.py
@@ -10,28 +10,54 @@ class TestRabbitListenerApp(TestCase):
     def tearDown(self):
         eventisc.default_app = None
 
-    @mock.patch.object(eventisc.rabbit_listener, "pika")
-    def test_http_listener(self, pika_mock):
-        app = eventisc.init_default_app(listeners=[
-            {
-                "event_name_regex": ".*",
-                "kind": "rabbit",
-                "url": "http://rabbit:5672/end",
-                "queue_kwargs": {"exchange": "some-exchange", "routing_key": "some-queue"},
-                "data": {
-                    "topic": "{event_data['topic']}",
-                    "action": "{event_data['action']}",
-                    "id": "{event_data['id']}"
+    def _default_rabbit_config(self):
+        return {
+            "event_name_regex": ".*",
+            "kind": "rabbit",
+            "url": "http://rabbit:5672/end",
+            "queue_kwargs": {
+                "exchange": {
+                    "exchange": "some-exchange",
+                    "exchange_type": "direct"
+                },
+                "queue": {
+                    "queue": "some-queue"
                 }
+            },
+            "publish_kwargs": {
+                "exchange": "{queue_kwargs['exchange']['exchange']}",
+                "routing_key": "{queue_kwargs['queue']['queue']}"
             }
-        ])
+        }
 
-        channel_mock = app.listeners[0].channel
+    @mock.patch.object(eventisc.rabbit_listener, "pika")
+    def test_rabbit_listener(self, pika_mock):
+        app = eventisc.init_default_app(listeners=[self._default_rabbit_config()])
+
+        channel_mock = eventisc.rabbit_listener.local.channel
 
         app.trigger('client_created', {"id": 14, "topic": "client", "action": "created"})
 
         channel_mock.basic_publish.assert_called_once_with(
             exchange='some-exchange',
             routing_key='some-queue',
-            body='{"topic": "client", "action": "created", "id": 14}'
+            body='{"id": 14, "topic": "client", "action": "created"}'
+        )
+
+    @mock.patch.object(eventisc.rabbit_listener, "pika")
+    def test_pika_connection(self, pika_mock):
+        eventisc.init_default_app(listeners=[self._default_rabbit_config()])
+
+        channel_mock = eventisc.rabbit_listener.local.channel
+
+        channel_mock.queue_declare.assert_called_once_with(queue="some-queue")
+
+        channel_mock.exchange_declare.assert_called_once_with(
+            exchange="some-exchange",
+            exchange_type="direct"
+        )
+
+        channel_mock.queue_bind.assert_called_once_with(
+            exchange="some-exchange",
+            queue="some-queue"
         )
